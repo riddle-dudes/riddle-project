e6e23faca8e736e3da0985fd4bc3009917f37227
Matt Bacher <mattjb1991@sbcglobal.net>
2017-11-04 14:10:47 -0400

    riddles in

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,241 @@
 INSERT INTO riddles(text, answer, level) VALUES
-("Test Riddle. Answer is dog. Level 1", "dog", 1),
-("Test Riddle. Answer is cat. Level 2", "cat", 2),
-("Test Riddle. Answer is mouse. Level 3", "mouse", 3),
-("Test Riddle. Answer is lard. Level 1", "lard", 1),
-("Test Riddle. Answer is magic. Level 2", "magic", 2),
-("Test Riddle. Answer is light. Level 3", "light", 3),
-("Test Riddle. Answer is keyboard. Level 1", "keyboard", 1),
-("Test Riddle. Answer is star. Level 2", "star", 2),
-("Test Riddle. Answer is hair. Level 3", "hair", 3);
+("What has a foot but no legs?", "snail", 1),
+("Poor people have it, rich people need it. If you eat it you die.", "nothing", 3),
+("What goes up but never comes down?", "rain", 2),
+("If I drink, I die. If I eat, I am fine. What am I?", "fire", 2),
+("If you have it, you don’t share it. If you share it, you don’t have it. What is it?", "secret", 2),
+("What word becomes shorter when you add two letters to it?", "short", 1),
+("My name states I'm a singular male, I can have a number, a head, but no tail. I've also great strength and I can be tough, but I'll smooth things out when they are rough. Although I go back to an age now gone, I'm around today for everyone.", "iron", 5),
+("I tremble at each breath of air, and yet can heaviest burdens bear.", "water", 5),
+("When young, I am sweet in the sun. When middle-aged, I make you happy. When old, I am valued more than ever.", "wine", 3),
+("I live through people and words. I can be found within buildings with steeples, and be among the birds. My first is in sea, but not in ocean. My last is in gallon, but not in quart. 
+	What am I?", "song", 5),
+("My 3, 10, 2 is a curse.
+My 9, 1, 6, 7 makes the three piece.
+My 3, 4, 6, 7, 1 makes waste.
+My 3, 5, 10 is my tint.
+My 8, 7 is a pronoun.
+My whole is very thorough.", "exhaustive", 4),
+("The bottom heats,
+The top cools,
+And little blobs
+Swim in my pools
+
+I can be different colours,
+And be a source of light,
+A bedroom accessory,
+I light up the night!
+
+What am I?", "volcano", 2),
+("I come in all shapes and sizes,
+But if you got me don't be expecting any surprises.
+You use me in a mall,
+But don't use me to call.
+I can be Texas or Sharp,
+But I can't play the harp.
+You can punch me all you want,
+And you may see me in a restaurant.
+I get what you want in an instant,
+And figure out some things you can't.
+Just select the parallel key,
+And the object you were searching for is what you will see.
+What am I?", "calculator", 1),
+("A hundred and one
+by fifty divide,
+And if a cipher
+is rightly applied,
+The answer is one from nine.", "clio", 5),
+("I have no fangs yet I constantly bite.
+I have no scales yet I slither through you.
+I can be delivered by eight legs, or no legs, and everywhere in between.
+I could cause death in minutes or be just a minor nuisance.
+
+What am I?", "venom", 3),
+("Maybe you eat me, perhaps you don't. Leave me alone too long and I'll get soggy, but either way I still float. 
+
+What am I?", "cheerios", 3),
+("The first letter in Whale but not in Hole,
+The second letter in Night but not in Toll,
+The third in Bless, oh yes, really,
+The fourth in Hello and in He.
+The whole is something that you make
+Before you eat a birthday cake.
+
+What is the word?
+", "wish", 1),
+("My first's last starts with S, and ends with Y,
+And oddly, my last's first does too.
+At the end of its last, it is sure to die,
+For my last first's to rise anew.
+
+The five between the first and last,
+Are not around when I awaken.
+If you think there is anything between last and first,
+Alas, you are sadly mistaken.
+
+My first's last, last is at my last's first last!
+And last of the five between,
+By the time you are through the last first,
+The first last will have been!", "weekend", 2),
+("He came through my window one beautiful night,
+He approached me,
+He touched me,
+He licked me,
+and he liked it,
+so he sucked me,
+he sucked me a lot,
+When he left,
+he left a mark,
+What was he?", "mosquito", 1),
+("My children come from eggs, though they are fed on milk.
+My gait resembles a lizard, but I surely am warmblooded.
+I am an expert swimmer because my feet are webbed,
+and my tail is a broad flat paddle.
+Yet, I still have claws, and my legs contain venom,
+whilst my tail is also a food store.
+My mouth looks like a duck though I cannot quack, and my voice growls like a dog.
+I am a most ingenious paradox.
+
+What am I?", "platypus", 2),
+("I am your friend who rings the bell DING-DING,
+Sometimes I even push you on the swing.
+I am probably where a person goes,
+Usually I go up your nose.
+Without me friend you'll probably die,
+
+Now I ask you what am I?", "air", 3),
+("I'm light as a feather, yet the strongest man can't hold me for more than 5 minutes. What am I?", "air", 1),
+("What goes in the water black and comes out red?", "lobster", 1),
+("Thirty men and only two women, but they hold the most power.
+Dressed in black and white, they could fight for hours.
+
+Who are they?", "chess pieces", 5),
+("Thirty white horses stand on a red hill,
+First they champ,
+Then they stamp,
+Then they stand still.", "teeth", 2),
+("I am an instrument that you can hear, but you can not touch or see me.
+
+What am I?", "voice", 1),
+("I have a name that's not mine,
+and no one cares about me in their prime.
+People cry at my sight,
+and lie by me all day and night.
+
+What am I?", "tombstone", 2),
+("I'm in everybody but everybody still wants me.
+I won't feed you but I'll feed the tree.", "water", 1),
+("Swallowing one is fine.
+Swallowing two together is fine.
+But Swallow them separately and all shall fall.", "magnets", 4),
+("I am a small room,
+but no life lives inside,
+no matter what weather looms,
+very cold my residents reside.
+What am I?", "refrigerator", 3),
+("I am the only organ that named myself. What am I?", "brain", 2),
+("A mirror for the famous, but informative to all.
+I'll show you the world, but it may be a bit small.
+
+What I am?", "tv", 2),
+("I am the beginning of the end, the end of every place. I am the beginning of eternity, the end of time and space. What am I?", "e", 1),
+("Different lights make me strange,
+for each one my size will change.
+
+What am I?", "pupil", 3),
+("What is thrown to both kings and beggars?", "toilet", 1),
+("I go up and I go down,
+towards the sky and the ground.
+I'm present and past tense too,
+Let's go for a ride, me and you.
+
+What am I?", "seesaw", 2),
+("When you do not know what I am, then I am something. But when you know what I am, then I am nothing. What am I?", "riddle", 1),
+("I have two arms, but fingers I have none. I've got two feet, but I cannot run. I carry well, but I carry best with my feet off the ground. What am I?", "wheelbarrow", 2),
+("What is something that you always have with you but you always leave behind?", "fingerprints", 1),
+("The person who buys me doesn't need me, the person who makes me doesn't want me, and the person who uses me can't appreciate me.
+
+What am I?", "coffin", 1),
+("The dirtier I am, the whiter I get,
+Leave a mark on me when you stand, I'll leave a mark on you when you sit.
+
+What am I?", "chalkboard", 2),
+("Under pressure is the only way I work,
+and by myself is the only way I'm hurt.
+
+What am I?", "diamond", 4),
+("What asks but never answers?", "owl", 2),
+("Sometimes I'm high and sometimes low,
+and I creep between your toes.
+My orders come from the sky,
+I make men fall and rise.
+
+What am I?", "tide", 3),
+("I talk, but I do not speak my mind
+I hear words, but I do not listen to thoughts
+When I wake, all see me
+When I sleep, all hear me
+Many heads are on my shoulders
+Many hands are at my feet
+The strongest steel cannot break my visage
+But the softest whisper can destroy me
+The quietest whimper can be heard.", "actor", 4),
+("I'm teary-eyed but never cry.
+Silver-tongued, but never lie.
+Double-winged, but never fly.
+Air-cooled, but never dry.
+
+What am I?", 'mercury', 5),
+('We hurt without moving. We poison without touching. We bear the truth and the lies. We are not to be judged by our size.What are we?', "words", 4),
+("Poke your fingers in my eyes and I will open wide my jaws. Linen cloth, quills, or paper, my greedy lust devours them all.
+
+What am I?", "scissors", 1),
+("If you're looking for something sweet I know what to do.
+But if you don't like heat I'm not for you.", "kitchen", 1),
+("When my first is a task to a young girl of spirit,
+And my second confines her to finish the piece,
+How hard is her fate! But how great is her merit
+If by taking my whole she effects her release!", "hemlock", 5),
+("If it's information you seek, come and see me.
+If it's pairs of letters you need, I have consecutively three.
+
+Who am I?", 'bookkeeper', 4),
+("A natural state, I'm sought by all.
+Go without me, and you shall fall.
+You do me when you spend,
+and use me when you eat to no end.
+
+What am I?", "balance", 5),
+("What is he that builds stronger than either the mason, the shipwright, or the carpenter?", "gravedigger", 5),
+("My first is often at the front door.
+My second is found in the cereal family.
+My third is what most people want.
+My whole is one of the united states.
+
+What am I?", "matrimony", 5),
+("I'm not a pool, a tree, a slave, a website, or a shirt.
+
+What am I?", 'book', 3),
+("500 is at my end and my start, yet 5 is at my heart.
+The first letter and the first number make me complete.
+My name is that of a king.
+
+What am I?", 'david', 5),
+("With pointed fangs I sit and wait; with piercing force I crunch out fate; grabbing victims, proclaiming might; physically joining with a single bite. What am I?", 'stapler', 1),
+("What goes up white and comes down yellow?", "egg", 2),
+("Past mountain, meadow, field, and hill, it follows a river while standing still.", 'riverbank', 2),
+("What has feet and legs, and nothing else?", 'pantyhose', 3),
+("What has no beginning, end, or middle?", 'doughnut', 2),
+("I C the C Above the C, what Do I C?", 'moon', 4),
+("You eat something you neither plant nor plow.
+It is the son of water, but if water touches it, it dies.", 'salt', 5),
+('I am an insect. Half of my name is another insect. What am I?', "beetle", 3),
+("I jump when I walk and sit when I stand. What am I?", 'kangaroo', 2),
+("I am not a toy, though many use me for pleasure. I have a butt but I can not poop. I have been known to kill people. What am I?", 'cigarette', 4),
+("Physicists have built devices to make me move quickly. My last eight letters can be commonly found in newspapers, magazines, and journals.
+
+What am I?", 'particles', 2),
+("What eight letter word has kst inside, in the beginning, and at the end?", "inkstand", 4);
 
 select * from riddles;
+
