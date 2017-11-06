19449604c8efe5c0831ddd1c6e28709e717ea515
Luke Keltner <lkeltner87@gmail.com>
2017-11-06 18:32:26 -0500

    works!

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ connection = mysql.createConnection(
 	port: 3306,
 
 	user: 'root',
-	password: 'password',
+	password: 'Lukkehoday1',
 	database: "game"
 });
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,21 @@
-INSERT INTO riddles(text, answer, level) VALUES
-("What has a foot but no legs?", "snail", 1),
-("Poor people have it, rich people need it. If you eat it you die.", "nothing", 3),
-("What goes up but never comes down?", "rain", 2),
-("If I drink, I die. If I eat, I am fine. What am I?", "fire", 2),
-("If you have it, you don’t share it. If you share it, you don’t have it. What is it?", "secret", 2),
-("What word becomes shorter when you add two letters to it?", "short", 1),
-("My name states I'm a singular male, I can have a number, a head, but no tail. I've also great strength and I can be tough, but I'll smooth things out when they are rough. Although I go back to an age now gone, I'm around today for everyone.", "iron", 5),
-("I tremble at each breath of air, and yet can heaviest burdens bear.", "water", 5),
-("When young, I am sweet in the sun. When middle-aged, I make you happy. When old, I am valued more than ever.", "wine", 3),
+INSERT INTO riddles(text, answer, level, correct, wrong) VALUES
+("What has a foot but no legs?", "snail", 1, 0, 0),
+("Poor people have it, rich people need it. If you eat it you die.", "nothing", 3, 0, 0),
+("What goes up but never comes down?", "rain", 2, 0, 0),
+("If I drink, I die. If I eat, I am fine. What am I?", "fire", 2, 0, 0),
+("If you have it, you don’t share it. If you share it, you don’t have it. What is it?", "secret", 2, 0, 0),
+("What word becomes shorter when you add two letters to it?", "short", 1, 0, 0),
+("My name states I'm a singular male, I can have a number, a head, but no tail. I've also great strength and I can be tough, but I'll smooth things out when they are rough. Although I go back to an age now gone, I'm around today for everyone.", "iron", 5, 0, 0),
+("I tremble at each breath of air, and yet can heaviest burdens bear.", "water", 5, 0, 0),
+("When young, I am sweet in the sun. When middle-aged, I make you happy. When old, I am valued more than ever.", "wine", 3, 0, 0),
 ("I live through people and words. I can be found within buildings with steeples, and be among the birds. My first is in sea, but not in ocean. My last is in gallon, but not in quart. 
-	What am I?", "song", 5),
+	What am I?", "song", 5, 0, 0),
 ("My 3, 10, 2 is a curse.
 My 9, 1, 6, 7 makes the three piece.
 My 3, 4, 6, 7, 1 makes waste.
 My 3, 5, 10 is my tint.
 My 8, 7 is a pronoun.
-My whole is very thorough.", "exhaustive", 4),
+My whole is very thorough.", "exhaustive", 4, 0, 0),
 ("The bottom heats,
 The top cools,
 And little blobs
@@ -26,7 +26,7 @@ And be a source of light,
 A bedroom accessory,
 I light up the night!
 
-What am I?", "volcano", 2),
+What am I?", "volcano", 2, 0, 0),
 ("I come in all shapes and sizes,
 But if you got me don't be expecting any surprises.
 You use me in a mall,
@@ -39,21 +39,21 @@ I get what you want in an instant,
 And figure out some things you can't.
 Just select the parallel key,
 And the object you were searching for is what you will see.
-What am I?", "calculator", 1),
+What am I?", "calculator", 1, 0, 0),
 ("A hundred and one
 by fifty divide,
 And if a cipher
 is rightly applied,
-The answer is one from nine.", "clio", 5),
+The answer is one from nine.", "clio", 5, 0, 0),
 ("I have no fangs yet I constantly bite.
 I have no scales yet I slither through you.
 I can be delivered by eight legs, or no legs, and everywhere in between.
 I could cause death in minutes or be just a minor nuisance.
 
-What am I?", "venom", 3),
+What am I?", "venom", 3, 0, 0),
 ("Maybe you eat me, perhaps you don't. Leave me alone too long and I'll get soggy, but either way I still float. 
 
-What am I?", "cheerios", 3),
+What am I?", "cheerios", 3, 0, 0),
 ("The first letter in Whale but not in Hole,
 The second letter in Night but not in Toll,
 The third in Bless, oh yes, really,
@@ -62,7 +62,7 @@ The whole is something that you make
 Before you eat a birthday cake.
 
 What is the word?
-", "wish", 1),
+", "wish", 1, 0, 0),
 ("My first's last starts with S, and ends with Y,
 And oddly, my last's first does too.
 At the end of its last, it is sure to die,
@@ -76,7 +76,7 @@ Alas, you are sadly mistaken.
 My first's last, last is at my last's first last!
 And last of the five between,
 By the time you are through the last first,
-The first last will have been!", "weekend", 2),
+The first last will have been!", "weekend", 2, 0, 0),
 ("He came through my window one beautiful night,
 He approached me,
 He touched me,
@@ -86,7 +86,7 @@ so he sucked me,
 he sucked me a lot,
 When he left,
 he left a mark,
-What was he?", "mosquito", 1),
+What was he?", "mosquito", 1, 0, 0),
 ("My children come from eggs, though they are fed on milk.
 My gait resembles a lizard, but I surely am warmblooded.
 I am an expert swimmer because my feet are webbed,
@@ -96,81 +96,81 @@ whilst my tail is also a food store.
 My mouth looks like a duck though I cannot quack, and my voice growls like a dog.
 I am a most ingenious paradox.
 
-What am I?", "platypus", 2),
+What am I?", "platypus", 2, 0, 0),
 ("I am your friend who rings the bell DING-DING,
 Sometimes I even push you on the swing.
 I am probably where a person goes,
 Usually I go up your nose.
 Without me friend you'll probably die,
 
-Now I ask you what am I?", "air", 3),
-("I'm light as a feather, yet the strongest man can't hold me for more than 5 minutes. What am I?", "air", 1),
-("What goes in the water black and comes out red?", "lobster", 1),
+Now I ask you what am I?", "air", 3, 0, 0),
+("I'm light as a feather, yet the strongest man can't hold me for more than 5 minutes. What am I?", "air", 1, 0, 0),
+("What goes in the water black and comes out red?", "lobster", 1, 0, 0),
 ("Thirty men and only two women, but they hold the most power.
 Dressed in black and white, they could fight for hours.
 
-Who are they?", "chess pieces", 5),
+Who are they?", "chess pieces", 5, 0, 0),
 ("Thirty white horses stand on a red hill,
 First they champ,
 Then they stamp,
-Then they stand still.", "teeth", 2),
+Then they stand still.", "teeth", 2, 0, 0),
 ("I am an instrument that you can hear, but you can not touch or see me.
 
-What am I?", "voice", 1),
+What am I?", "voice", 1, 0, 0),
 ("I have a name that's not mine,
 and no one cares about me in their prime.
 People cry at my sight,
 and lie by me all day and night.
 
-What am I?", "tombstone", 2),
+What am I?", "tombstone", 2, 0, 0),
 ("I'm in everybody but everybody still wants me.
-I won't feed you but I'll feed the tree.", "water", 1),
+I won't feed you but I'll feed the tree.", "water", 1, 0, 0),
 ("Swallowing one is fine.
 Swallowing two together is fine.
-But Swallow them separately and all shall fall.", "magnets", 4),
+But Swallow them separately and all shall fall.", "magnets", 4, 0, 0),
 ("I am a small room,
 but no life lives inside,
 no matter what weather looms,
 very cold my residents reside.
-What am I?", "refrigerator", 3),
-("I am the only organ that named myself. What am I?", "brain", 2),
+What am I?", "refrigerator", 3, 0, 0),
+("I am the only organ that named myself. What am I?", "brain", 2, 0, 0),
 ("A mirror for the famous, but informative to all.
 I'll show you the world, but it may be a bit small.
 
-What I am?", "tv", 2),
-("I am the beginning of the end, the end of every place. I am the beginning of eternity, the end of time and space. What am I?", "e", 1),
+What I am?", "tv", 2, 0, 0),
+("I am the beginning of the end, the end of every place. I am the beginning of eternity, the end of time and space. What am I?", "e", 1, 0, 0),
 ("Different lights make me strange,
 for each one my size will change.
 
-What am I?", "pupil", 3),
-("What is thrown to both kings and beggars?", "toilet", 1),
+What am I?", "pupil", 3, 0, 0),
+("What is thrown to both kings and beggars?", "toilet", 1, 0, 0),
 ("I go up and I go down,
 towards the sky and the ground.
 I'm present and past tense too,
 Let's go for a ride, me and you.
 
-What am I?", "seesaw", 2),
-("When you do not know what I am, then I am something. But when you know what I am, then I am nothing. What am I?", "riddle", 1),
-("I have two arms, but fingers I have none. I've got two feet, but I cannot run. I carry well, but I carry best with my feet off the ground. What am I?", "wheelbarrow", 2),
-("What is something that you always have with you but you always leave behind?", "fingerprints", 1),
+What am I?", "seesaw", 2, 0, 0),
+("When you do not know what I am, then I am something. But when you know what I am, then I am nothing. What am I?", "riddle", 1, 0, 0),
+("I have two arms, but fingers I have none. I've got two feet, but I cannot run. I carry well, but I carry best with my feet off the ground. What am I?", "wheelbarrow", 2, 0, 0),
+("What is something that you always have with you but you always leave behind?", "fingerprints", 1, 0, 0),
 ("The person who buys me doesn't need me, the person who makes me doesn't want me, and the person who uses me can't appreciate me.
 
-What am I?", "coffin", 1),
+What am I?", "coffin", 1, 0, 0),
 ("The dirtier I am, the whiter I get,
 Leave a mark on me when you stand, I'll leave a mark on you when you sit.
 
-What am I?", "chalkboard", 2),
+What am I?", "chalkboard", 2, 0, 0),
 ("Under pressure is the only way I work,
 and by myself is the only way I'm hurt.
 
-What am I?", "diamond", 4),
-("What asks but never answers?", "owl", 2),
+What am I?", "diamond", 4, 0, 0),
+("What asks but never answers?", "owl", 2, 0, 0),
 ("Sometimes I'm high and sometimes low,
 and I creep between your toes.
 My orders come from the sky,
 I make men fall and rise.
 
-What am I?", "tide", 3),
+What am I?", "tide", 3, 0, 0),
 ("I talk, but I do not speak my mind
 I hear words, but I do not listen to thoughts
 When I wake, all see me
@@ -179,143 +179,143 @@ Many heads are on my shoulders
 Many hands are at my feet
 The strongest steel cannot break my visage
 But the softest whisper can destroy me
-The quietest whimper can be heard.", "actor", 4),
+The quietest whimper can be heard.", "actor", 4, 0, 0),
 ("I'm teary-eyed but never cry.
 Silver-tongued, but never lie.
 Double-winged, but never fly.
 Air-cooled, but never dry.
 
-What am I?", 'mercury', 5),
-('We hurt without moving. We poison without touching. We bear the truth and the lies. We are not to be judged by our size.What are we?', "words", 4),
+What am I?", 'mercury', 5, 0, 0),
+('We hurt without moving. We poison without touching. We bear the truth and the lies. We are not to be judged by our size.What are we?', "words", 4, 0, 0),
 ("Poke your fingers in my eyes and I will open wide my jaws. Linen cloth, quills, or paper, my greedy lust devours them all.
 
-What am I?", "scissors", 1),
+What am I?", "scissors", 1, 0, 0),
 ("If you're looking for something sweet I know what to do.
-But if you don't like heat I'm not for you.", "kitchen", 1),
+But if you don't like heat I'm not for you.", "kitchen", 1, 0, 0),
 ("When my first is a task to a young girl of spirit,
 And my second confines her to finish the piece,
 How hard is her fate! But how great is her merit
-If by taking my whole she effects her release!", "hemlock", 5),
+If by taking my whole she effects her release!", "hemlock", 5, 0, 0),
 ("If it's information you seek, come and see me.
 If it's pairs of letters you need, I have consecutively three.
 
-Who am I?", 'bookkeeper', 4),
+Who am I?", 'bookkeeper', 4, 0, 0),
 ("A natural state, I'm sought by all.
 Go without me, and you shall fall.
 You do me when you spend,
 and use me when you eat to no end.
 
-What am I?", "balance", 5),
-("What is he that builds stronger than either the mason, the shipwright, or the carpenter?", "gravedigger", 5),
+What am I?", "balance", 5, 0, 0),
+("What is he that builds stronger than either the mason, the shipwright, or the carpenter?", "gravedigger", 5, 0, 0),
 ("My first is often at the front door.
 My second is found in the cereal family.
 My third is what most people want.
 My whole is one of the united states.
 
-What am I?", "matrimony", 5),
+What am I?", "matrimony", 5, 0, 0),
 ("I'm not a pool, a tree, a slave, a website, or a shirt.
 
-What am I?", 'book', 3),
+What am I?", 'book', 3, 0, 0),
 ("500 is at my end and my start, yet 5 is at my heart.
 The first letter and the first number make me complete.
 My name is that of a king.
 
-What am I?", 'david', 5),
-("With pointed fangs I sit and wait; with piercing force I crunch out fate; grabbing victims, proclaiming might; physically joining with a single bite. What am I?", 'stapler', 1),
-("What goes up white and comes down yellow?", "egg", 2),
-("Past mountain, meadow, field, and hill, it follows a river while standing still.", 'riverbank', 2),
-("What has feet and legs, and nothing else?", 'pantyhose', 3),
-("What has no beginning, end, or middle?", 'doughnut', 2),
-("I C the C Above the C, what Do I C?", 'moon', 4),
+What am I?", 'david', 5, 0, 0),
+("With pointed fangs I sit and wait; with piercing force I crunch out fate; grabbing victims, proclaiming might; physically joining with a single bite. What am I?", 'stapler', 1, 0, 0),
+("What goes up white and comes down yellow?", "egg", 2, 0, 0),
+("Past mountain, meadow, field, and hill, it follows a river while standing still.", 'riverbank', 2, 0, 0),
+("What has feet and legs, and nothing else?", 'pantyhose', 3, 0, 0),
+("What has no beginning, end, or middle?", 'doughnut', 2, 0, 0),
+("I C the C Above the C, what Do I C?", 'moon', 4, 0, 0),
 ("You eat something you neither plant nor plow.
-It is the son of water, but if water touches it, it dies.", 'salt', 5),
-('I am an insect. Half of my name is another insect. What am I?', "beetle", 3),
-("I jump when I walk and sit when I stand. What am I?", 'kangaroo', 2),
-("I am not a toy, though many use me for pleasure. I have a butt but I can not poop. I have been known to kill people. What am I?", 'cigarette', 4),
+It is the son of water, but if water touches it, it dies.", 'salt', 5, 0, 0),
+('I am an insect. Half of my name is another insect. What am I?', "beetle", 3, 0, 0),
+("I jump when I walk and sit when I stand. What am I?", 'kangaroo', 2, 0, 0),
+("I am not a toy, though many use me for pleasure. I have a butt but I can not poop. I have been known to kill people. What am I?", 'cigarette', 4, 0, 0),
 ("Physicists have built devices to make me move quickly. My last eight letters can be commonly found in newspapers, magazines, and journals.
 
-What am I?", 'particles', 2),
-("What eight letter word has kst inside, in the beginning, and at the end?", "inkstand", 4),
+What am I?", 'particles', 2, 0, 0),
+("What eight letter word has kst inside, in the beginning, and at the end?", "inkstand", 4, 0, 0),
 ("When you need me, you throw me away. But when you're done with me, you bring me back.
 
-What am I?", 'anchor', 4),
+What am I?", 'anchor', 4, 0, 0),
 ("Many have heard me, but nobody has seen me, and I will not speak back until spoken to.
-What am I?", 'echo', 2),
+What am I?", 'echo', 2, 0, 0),
 ("I fly, yet I have no wings. I cry, yet I have no eyes. Darkness follows me; lower light I never see.
 
-What am I?", "cloud", 3),
+What am I?", "cloud", 3, 0, 0),
 ("You struggle to regain me.
 When I'm lost, you struggle to obtain me.
 I pass no matter your will,
 but I'm your slave to kill.
-What am I?", "time", 4),
+What am I?", "time", 4, 0, 0),
 ("Feathers, I am as light,
 But quite heavy if squeezed tight.
 I'm seen as clean and pure,
 but often talked about with dirty terms.
-What am I?", "snow", 2),
+What am I?", "snow", 2, 0, 0),
 ("It dances and skips,
 it's read in the eyes but it cheats with the hips,
 If it meets its match it's easily caught,
 but it's worth nothing if it is bought.
-What is it?", "heart", 3),
+What is it?", "heart", 3, 0, 0),
 ("The more places I be, the less you can see.
 
-What am I?", 'darkness', 1),
+What am I?", 'darkness', 1, 0, 0),
 ("I can be used to build castles,
 but I crumble in your hands.
 I can help a man see,
-and am found all around the lands.", 'sand', 1),
-("I can bring tears to your eyes; resurrect the dead, make you smile, and reverse time. I form in an instant but I last a life time. What am I?", 'memory', 5),
+and am found all around the lands.", 'sand', 1, 0, 0),
+("I can bring tears to your eyes; resurrect the dead, make you smile, and reverse time. I form in an instant but I last a life time. What am I?", 'memory', 5, 0, 0),
 ("Skinny I am fast,
 fat I am slow,
 but I'll still delight you from your eyes to your nose.
 
-What am I?", 'candle', 1),
+What am I?", 'candle', 1, 0, 0),
 ("I'll bring out your bulls,
 and slow your roll.
 I may even come out of your skull.
 
-What am I?", 'red', 1),
+What am I?", 'red', 1, 0, 0),
 ("It brings light to a dark dark world,
 In a current it travels and through tunnels it's hurled.
 Touch it and it'll kill you.
 
-What is it?", 'electricity', 4),
+What is it?", 'electricity', 4, 0, 0),
 ("You put me in hard.
 But then I become soft.
 Blow me hard and I will get bigger.
 
-What am I?", 'gum', 2),
+What am I?", 'gum', 2, 0, 0),
 ("No matter how hard you try,
 I can open what you can't.
 If it wasn't for me,
 on your front porch you would stand.
 
-What am I?", 'key', 1),
+What am I?", 'key', 1, 0, 0),
 ("You put me in dry but then I get wet.
 The longer I stay in, the stronger it will get.
 
-What am I?", 'teabag', 2),
+What am I?", 'teabag', 2, 0, 0),
 ("I have lakes with no fish.
 I have roads with no cars.
 
-What am I?", 'map', 1),
+What am I?", 'map', 1, 0, 0),
 ("I can be sun,
 I can be sand,
 and I can be bird.
 
-What am I?", "clock", 3),
+What am I?", "clock", 3, 0, 0),
 ("I used to fill rooms but now I'm a tower,
 I can stay put or I can travel far.
 
-What am I?", 'computer', 1),
-("We have what we did not find; what we did find we left behind.", 'lice', 2),
-("I never was. Am always to be. No one ever saw me, nor ever will. And yet I am the confidence of all who live and breathe. What am I ?", 'tomorrow', 5),
-("Whats 3/7 chicken, 2/3 cat, and 2/4 goat?", 'chicago', 2),
-("I weaken all men for hours each day. I show you strange visions while you are away. I take you by night, by day take you back, None suffer to have me, but do from my lack. What am I?", 'sleep', 2),
-("I'm simple for a few people But hard for them to hear I live inside of secrets I bring people's worst fears What am I?", 'truth', 4),
-("What is easy to get into, but hard to get out of?", 'trouble', 1);
+What am I?", 'computer', 1, 0, 0),
+("We have what we did not find; what we did find we left behind.", 'lice', 2, 0, 0),
+("I never was. Am always to be. No one ever saw me, nor ever will. And yet I am the confidence of all who live and breathe. What am I ?", 'tomorrow', 5, 0, 0),
+("Whats 3/7 chicken, 2/3 cat, and 2/4 goat?", 'chicago', 2, 0, 0),
+("I weaken all men for hours each day. I show you strange visions while you are away. I take you by night, by day take you back, None suffer to have me, but do from my lack. What am I?", 'sleep', 2, 0, 0),
+("I'm simple for a few people But hard for them to hear I live inside of secrets I bring people's worst fears What am I?", 'truth', 4, 0, 0),
+("What is easy to get into, but hard to get out of?", 'trouble', 1, 0, 0);
 
 select * from riddles;
 
